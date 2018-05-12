9cbd4503190be5d4ade7bcdb7a7eb96e6490ba69
the_next <543584502@qq.com>
2018-05-12 17:29:51 +0800

    [cpp lexi] 添加语法解析器对自定义token的支持

--- include/lexicalAnalysier.hpp
+++ include/lexicalAnalysier.hpp
@@ -3,25 +3,45 @@
 #include <string>       // ::std::string
 #include <map>          // ::std::map
 #include <vector>       // ::std::vector
+#include <memory>       // ::std::shared_ptr
+#include "struct/token.hpp"
+
 
 namespace theNext {
 /**
  * @brief 词法分析器
+ *
+ * @tparam T 类型定义器
+ *      类型定义器中间应当全是static const ::std::string
  */
+template<class TYPE>
 class lexicalAnalysier {
 public:
     // 类型定义
-    typedef const ::std::string type_t;
+    typedef ::theNext::token_type type_t;
     // 分析内容定义
     typedef ::std::string text_t;
     // 状态转换函数定义
-    typedef ::std::function<type_t(text_t::iterator &, text_t::iterator)> state_change_t;
+    typedef ::std::function<TYPE(text_t::iterator &, text_t::iterator)> state_change_t;
+    // 判断终止（错误或者判断结束）回调函数
+    typedef ::std::function < token(text_t, TYPE)> end_callback_t;
+    // 本体类型
+    typedef theNext::lexicalAnalysier<TYPE> this_t;
 public:
     // 设置用函数
-    lexicalAnalysier(/* args */) {};
-    ~lexicalAnalysier() {};
-private:
-};
 
+    lexicalAnalysier() {};
+    ~lexicalAnalysier() {};
+
+    this_t &addStateChangeFunc(TYPE type, const state_change_t &func) {
+        this->state_change_list[type].push_back(func);
+        return *this;
+    }
+private:
+    // 状态转换函数列表
+    ::std::map<type_t, ::std::vector<state_change_t> > state_change_list;
+    // 当前状态
+    TYPE now_state;
+};
 
 } // namespace theNext
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "lexicalAnalysier.hpp"
 
-int main(int, char**) {
-   std::cout << "Hello, world!\n";
+int main(int, char **) {
+    // ::theNext::lexicalAnalysier<::theNext::token_type> a;
+    std::cout << "Hello, world!\n";
 }
