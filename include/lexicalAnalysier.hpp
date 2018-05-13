29d432ef90e9be11a0aace79819d98463ee9a963
the_next <543584502@qq.com>
2018-05-13 16:57:40 +0800

    [cpp lexi]放弃采用函数方式，改用直接的map类型描述结点

--- include/lexicalAnalysier.hpp
+++ include/lexicalAnalysier.hpp
@@ -8,40 +8,79 @@
 
 
 namespace theNext {
+
 /**
- * @brief 词法分析器
+ * @brief 建立简单的词法分析器
  *
- * @tparam T 类型定义器
- *      类型定义器中间应当全是static const ::std::string
+ * @tparam TYPE 继承于::theNext::token_type，默认构造函数为初始状态,设置友元类
  */
 template<class TYPE>
 class lexicalAnalysier {
 public:
     // 类型定义
     typedef ::theNext::token_type type_t;
+    typedef ::theNext::token<TYPE> token;
     // 分析内容定义
     typedef ::std::string text_t;
     // 状态转换函数定义
-    typedef ::std::function<TYPE(text_t::iterator &, text_t::iterator)> state_change_t;
-    // 判断终止（错误或者判断结束）回调函数
-    typedef ::std::function < token(text_t, TYPE)> end_callback_t;
+    typedef ::std::pair<std::pair<TYPE, char>, TYPE> state_change_t;
+    // 结束方式
+    typedef ::std::pair<TYPE, type_t> end_state_t;
     // 本体类型
     typedef theNext::lexicalAnalysier<TYPE> this_t;
 public:
     // 设置用函数
-
-    lexicalAnalysier() {};
+    lexicalAnalysier() {}
     ~lexicalAnalysier() {};
-
-    this_t &addStateChangeFunc(TYPE type, const state_change_t &func) {
-        this->state_change_list[type].push_back(func);
+    /**
+     * @brief 设置状态转换
+     *
+     * @param nowState 当前状态
+     * @param nowChar 状态下出现的字符
+     * @param nextState
+     * @return this_t&
+     */
+    this_t &addStateChangeWay(TYPE nowState, char nowChar, TYPE nextState) {
+        this->state_change_map[::std::make_pair(nowState, nowChar)] = nextState;
         return *this;
+    }
+    /**
+     * @brief 定义一个状态的结束状态
+     * 在一个自动机结束（没有相应的匹配）的时候调用
+     * 判断当前缓冲中的字符串的类型
+     * @param nowState 当前状态
+     * @param outState 结束后当前字符的类型
+     * @return this_t&
+     */
+    this_t &defineEndState(TYPE nowState, TYPE outState) {
+        this->end_state[nowState] = nowState;
+        return *this;
+    }
+public:
+    /**
+     * @brief 分析函数
+     *
+     * @param context 分析的文章
+     */
+    ::std::vector<token> analysis(::std::string context) {
+        ::std::vector<token> ans;
+        TYPE now_state;
+        for(auto it = context.begin(); it != context.end(); ++it) {
+            if(this->query_state(now_state, *it) != this->state_change_map.end()) {
+                now_state = *query_state(now_state,*it);
+            }
+        }
+        return ans;
+    }
+protected:
+    auto query_state(TYPE type, char a) {
+        return this->state_change_map.find(::std::make_pair(type, a));
     }
 private:
     // 状态转换函数列表
-    ::std::map<type_t, ::std::vector<state_change_t> > state_change_list;
-    // 当前状态
-    TYPE now_state;
+    ::std::map<std::pair<TYPE, char>, TYPE> state_change_map;
+    // 结束处理列表
+    ::std::map<TYPE, TYPE> end_state;
 };
 
 } // namespace theNext
