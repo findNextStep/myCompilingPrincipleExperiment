683d5e6120fdd77e43b9be4de07e0ff6b258a9ce
the_next <543584502@qq.com>
2018-05-14 11:51:30 +0800

    [cpp test]建立测试样例，通过编译

--- main.cpp
+++ main.cpp
@@ -7,5 +7,10 @@ using ::theNext::threadSetting::threadState;
 int main(int, char **) {
     ::theNext::lexicalAnalysier<threadState> a;
     a.addStateChangeWay(threadState(), '+', threadState::addOp);
+    ::nlohmann::json json;
+    json["type"] = "test";
+    json["content"] = "dafda";
+    auto dafdafd = ::theNext::token::fromJson(json);
+    std::cout << dafdafd.toJson() << std::endl;
     return 0;
 }
