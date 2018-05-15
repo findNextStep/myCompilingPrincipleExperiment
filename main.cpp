b0134681c755b6372b6d28d704063ffba1aef2e1
the_next <543584502@qq.com>
2018-05-15 11:36:27 +0800

    [cpp test]添加检查方式

--- main.cpp
+++ main.cpp
@@ -3,14 +3,22 @@
 #include "threadLanguage/threadSetting.hpp"
 
 using ::theNext::threadSetting::threadState;
+using namespace theNext;
+using namespace std;
 
 int main(int, char **) {
-    ::theNext::lexicalAnalysier<threadState> a;
-    a.addStateChangeWay(threadState(), '+', threadState::addOp);
-    ::nlohmann::json json;
-    json["type"] = "test";
-    json["content"] = "dafda";
-    auto dafdafd = ::theNext::token::fromJson(json);
-    std::cout << dafdafd.toJson() << std::endl;
+    ::theNext::lexicalAnalysier<threadState> analise = theNext::threadSetting::getlex();
+    for(auto item : analise.state_change_map) {
+        cout << item.first.first.getType() << " --\"" << item.first.second << "\"-->" << item.second.getType() << endl;
+    }
+    string input;
+    while(cin >> input) {
+        auto ans = analise.analysis(input);
+        cout << "end";
+        for(auto a : ans) {
+            cout << a.toJson() << endl;
+        }
+
+    }
     return 0;
 }
