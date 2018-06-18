0e6f8f348256ea6fb88516b93a25689a597526f2
findNextStep <543584502@qq.com>
2018-06-18 18:53:10 +0800

    [cpp tgra]为当前的项目结构变化更改测试用例的代码

--- src/test/grammaticalAnalysierTest.cpp
+++ src/test/grammaticalAnalysierTest.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <tool/treePrinter.hpp>
 
 using namespace theNext;
 using namespace std;
@@ -93,71 +94,15 @@ void printTree(const ::nlohmann::json &js, vector<string> need_list, int my = 0)
 }
 
 int main() {
-    grammaticalAnalysier ana;
-    ana.addGramaticRule("ThreadSpec",
-                        rule_t({"thread", "identifier"}),
-                        optional({"features", "featureSpec"}),
-                        optional({"flows", "flowSpec"}),
-                        optional({"properties", "association", ";"}),
-                        rule_t({"end", "identifier", ";"}));
-    ana.addGramaticRule("featureSpec", rule_t({"portSpec"}));
-    ana.addGramaticRule("featureSpec", rule_t({"ParameterSpec"}));
-    ana.addGramaticRule("featureSpec", rule_t({"none"}));
-    ana.addGramaticRule("portSpec",
-                        rule_t({"identifier", ":", "IOtype", "portType"}),
-                        option_and_repeat({"{", "association", "}"}),
-                        rule_t({";"}));
-    ana.addGramaticRule("portType",
-                        rule_t({"data", "port"}),
-                        optional({"reference"}));
-    ana.addGramaticRule("portType",
-                        rule_t({"event", "data", "port"}),
-                        optional({"reference"}));
-
-    ana.addGramaticRule("portType",
-                        rule_t({"event", "port"}));
-    ana.addGramaticRule("ParameterSpec",
-                        rule_t({"identifier", ":", "IOtype", "parameter" }),
-                        optional({"reference"}),
-                        option_and_repeat({"{", "association", "}"}),
-                        rule_t({";"}));
-    ana.addGramaticRule("IOtype", rule_t({"in"}));
-    ana.addGramaticRule("IOtype", rule_t({"out"}));
-    ana.addGramaticRule("IOtype", rule_t({"in", "out"}));
-    ana.addGramaticRule("flowSpec", rule_t({"flowSourceSpec"}));
-    ana.addGramaticRule("flowSpec", rule_t({"flowSinkSpec"}));
-    ana.addGramaticRule("flowSpec", rule_t({"flowPathSpec"}));
-    ana.addGramaticRule("flowSpec", rule_t({"none", ";"}));
-    ana.addGramaticRule("flowSourceSpec", rule_t({"identifier", ":", "flow", "source", "identifier"}),
-                        option_and_repeat({"{", "association", "}"}),
-                        rule_t({";"}));
-    ana.addGramaticRule("flowSinkSpec",
-                        rule_t({"identifier", ":", "flow", "source", "identifier"}),
-                        option_and_repeat({"association"}));
-    ana.addGramaticRule("flowPathSpec", rule_t({"identifier", ":", "flow", "path", "identifier", "->", "identifier", ";"}));
-    ana.addGramaticRule("association", optional({"identifier", "::"}),
-                        rule_t({"identifier", "splitter"}),
-                        optional({"constant"}),
-                        rule_t({"access", "decimal"}));
-    ana.addGramaticRule("association", rule_t({"none"}));
-    ana.addGramaticRule("splitter", rule_t({"+=>"}));
-    ana.addGramaticRule("splitter", rule_t({"=>"}));
-    ana.addGramaticRule("reference",
-                        optional({"packageName"}),
-                        rule_t({"identifier"}));
-
-    ana.addGramaticRule("packageName",
-                        rule_t({"identifier", "::"})
-                       );
-
-
     auto lex = theNext::threadSetting::getlex();
+    auto ana = theNext::threadSetting::getgra();
 
     std::string path;
     std::cin >> path;
     auto content = readFile(path);
-    cout << ana.analise(lex.analysis(content)).toJson() << endl;
+    cout << ana.analise(lex.analysis(content)).toJson().dump(2) << endl;
     auto ans = ana.analise(lex.analysis(content));
-    printTree(ans.toJson(), {threadSetting::identifier, threadSetting::decimal});
+    theNext::treePrinter printer({threadSetting::identifier, threadSetting::decimal});
+    printer.setTree(ans.toJson());
     return 0;
 }
