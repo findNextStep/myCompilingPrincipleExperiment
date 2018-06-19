3169da9dc95d0ebe92c062378fe0307beed4e05b
findNextStep <543584502@qq.com>
2018-06-19 14:42:38 +0800

    [cpp tool]添加注释

--- include/struct/gramaticalTree.hpp
+++ include/struct/gramaticalTree.hpp
@@ -5,28 +5,55 @@
 #include <nlohmann/json.hpp>
 
 namespace theNext {
-
+/**
+ * @brief 语法结构输出
+ * 
+ */
 struct gramaticalTree {
 private:
     std::vector<gramaticalTree> son;
 public:
     token my;
+    /**
+     * @brief 初始化结构，使用输入参数作为结点类型
+     * 
+     * @param name 类型
+     */
     gramaticalTree(::std::string name) {
         this->my.type = name;
     }
+    /**
+     * @brief 通过直接添加一个token实现初始化
+     * 
+     * @param toke token本身
+     */
     gramaticalTree(token toke): my(toke) { }
     ~gramaticalTree() { }
-
+    /**
+     * @brief 添加一个子树
+     * 
+     * @param son 子树
+     * @return gramaticalTree& this
+     */
     gramaticalTree &addSon(const gramaticalTree &son) {
         this->son.push_back(son);
         return *this;
     }
-
+    /**
+     * @brief 在队头添加一个子树
+     * 
+     * @param son 需要添加的子树
+     * @return gramaticalTree& this
+     */
     gramaticalTree &addSon_back(const gramaticalTree &son) {
         this->son.insert(this->son.begin(), son);
         return *this;
     }
-
+    /**
+     * @brief 转换为json格式
+     * 
+     * @return nlohmann::json 
+     */
     nlohmann::json toJson()const {
         auto ans = this->my.toJson();
         for(auto s : son) {
--- include/tool/treePrinter.hpp
+++ include/tool/treePrinter.hpp
@@ -10,22 +10,75 @@ public:
     typedef ::nlohmann::json json;
     typedef ::std::vector<std::string> passList_t;
 private:
+    /**
+     * @brief 需要显示的树的内容
+     */
     ::nlohmann::json tree;
+    /**
+     * @brief 需要输出的结点类型
+     *
+     */
     passList_t pass_list;
 protected:
-    bool static need_to_print(const json &, const std::vector<std::string> &);
-    json static remove_unneed(const json &, const std::vector<std::string> &);
-    std::string static printTree(const json &, const int level, const int index);
-    void init(const json &);
+    /**
+     * @brief 判断一个结点是否需要输出
+     * 如果一个结点本身的类型需要输出，或者其子树需要输出
+     * 则这个结点需要输出
+     * @param js 需要输出的结点
+     * @param list 需要输出的类型名称列表
+     * @return true 这个结点需要输出
+     * @return false 这个结点不需要输出
+     */
+    bool static need_to_print(const json &js, const std::vector<std::string> &list);
+    /**
+     * @brief 删除子树中所有的不需要的子树
+     * 需要通过need_to_print判断
+     * @param js 进行处理的结点
+     * @param list 可以输出的类型名列表
+     * @return json 消除的内容
+     */
+    json static remove_unneed(const json &js, const std::vector<std::string> &list);
+    /**
+     * @brief 对当前的js递归的通过缩进格式化
+     *
+     * @param js 需要输出的js
+     * @param level 当前需要的缩进等级
+     * @param index 缩进的格子数
+     * @return std::string 格式化之后的结果
+     */
+    std::string static printTree(const json &js, const int level, const int index);
+    /**
+     * @brief 初始化tree
+     *
+     * @param js
+     */
+    void init(const json &js);
 public:
     treePrinter(const passList_t &passlist);
     ~treePrinter() {};
+    /**
+     * @brief 设置自身的js内容
+     *
+     * @param tree
+     */
     void setTree(json tree) {
         return this->init(tree);
     }
+    /**
+     * @brief 直接返回js格式化输出后的结果
+     *
+     * @param index 缩进比例
+     * @return std::string
+     */
     std::string toJson(int index = 4) {
         return this->tree.dump(index);
     }
+    /**
+     * @brief 通过缩进格式化输出
+     *
+     * @param index 格式化每一个的缩进量
+     * @return std::string
+     */
     std::string toList(int index = 4);
 };
 } // namespace theNext
